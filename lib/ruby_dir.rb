693128cffb7f7f657ec9d01c08cde0876d51fed0
matt-mcalister <matt.mcalister93@gmail.com>
2019-01-30 12:43:16 -0500

    created basic directory structure with ruby_dir NAME command

--- lib/ruby_dir.rb
+++ lib/ruby_dir.rb
@@ -3,8 +3,26 @@ require "ruby_dir/version"
 module RubyDir
   class Error < StandardError; end
   class Instruct
+      attr_reader :name
+
+      def initialize(name)
+        @name = name
+      end
+
       def create_directory
-        puts "creating directory!"
+        `mkdir #{name}`
+        puts "created #{name}/"
+        `cd #{name} && mkdir app`
+        puts "created #{name}/app"
+        `cd #{name} && bundle init`
+        puts "created #{name}/Gemfile"
+        `cd #{name} && echo '\ngem "rspec"\ngem "require_all"\ngem "pry"' >> Gemfile`
+        puts "added pry and rspec to Gemfile"
+        `cd #{name} && rspec --init`
+        `cd #{name} && mkdir config && touch config/environment.rb`
+        `cd #{name} && echo "require 'bundler/setup'\nBundler.require\nrequire_rel '../app'" >> config/environment.rb`
+        puts "created #{name}/config/environment.rb"
+        `cd #{name} && echo 'require_relative "../config/environment.rb"' | cat - spec/spec_helper.rb > temp && mv temp spec/spec_helper.rb`
       end
     end
 end
