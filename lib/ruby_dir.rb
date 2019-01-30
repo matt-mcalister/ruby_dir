a2a7aa95f4e3b02b0fca29dfd58c2a928327b064
matt-mcalister <matt.mcalister93@gmail.com>
2019-01-30 17:41:30 -0500

    setup config/tools.rb

--- lib/ruby_dir.rb
+++ lib/ruby_dir.rb
@@ -18,11 +18,16 @@ module RubyDir
         puts "created #{name}/Gemfile"
         `cd #{name} && echo '\ngem "rspec"\ngem "require_all"\ngem "pry"' >> Gemfile`
         puts "added pry and rspec to Gemfile"
-        `cd #{name} && rspec --init`
         `cd #{name} && mkdir config && touch config/environment.rb`
         `cd #{name} && echo "require 'bundler/setup'\nBundler.require\nrequire_rel '../app'" >> config/environment.rb`
         puts "created #{name}/config/environment.rb"
+        `cd #{name} && rspec --init`
         `cd #{name} && echo 'require_relative "../config/environment.rb"' | cat - spec/spec_helper.rb > temp && mv temp spec/spec_helper.rb`
+        puts "created #{name}/rspec/spec_helper.rb"
+        `cd #{name} && mkdir tools && touch tools/console.rb`
+        `echo "require_relative '../config/environment.rb'\n\ndef reload\n  load 'config/environment.rb'\nend\n\n# Insert code here to run before hitting the binding.pry\n# This is a convenient place to define variables and/or set up new object instances,\n# so they will be available to test and play around with in your console\n\nbinding.pry\nputs 'goodbye'" >> #{name}/tools/console.rb`
+        puts "created #{name}/tools/console.rb"
+        `cd #{name} && bundle`
       end
     end
 end
